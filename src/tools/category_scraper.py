4d6b98e5d637fc6fc49ffdfded367b1fe6487a09
Magdalena Strzoda <magdalena.strzoda@myeatclub.com>
2017-11-05 18:17:47 -0800

    Cleanup: remove old and unused files.

--- src/tools/category_scraper.py
+++ src/tools/category_scraper.py
@@ -102,11 +102,11 @@ class CategoryScraper:
             categories = self.get_categories(query)
             self.save_to_file(categories, append=False)
         else:
-            # TODO: test
             # TODO: load previously saved categories
+            self.subcategories_to_visit = set(filter(lambda x: self.category_matcher.is_category_related(x), self.subcategories_to_visit))
             categories = set()
-            subcategories_to_visit = {category for category in self.subcategories_to_visit}
-            for category in subcategories_to_visit:
+            # subcategories_to_visit = {category for category in self.subcategories_to_visit}
+            for category in self.subcategories_to_visit:
                 query = self.get_default_query(category)
                 new_categories = self.get_categories(query)
                 categories.update(new_categories)
