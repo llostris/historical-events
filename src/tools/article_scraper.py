290665e203a45a219449629d7b86329dd9327936
Magdalena Strzoda <deiidrell@gmail.com>
2018-04-21 13:07:41 +0200

    Fix loading old pickled RawArticles.

---
+++ src/tools/article_scraper.py
@@ -0,0 +1,7 @@
+"""
+ArticleScraper and RawArticle has been moved to tools.download.
+
+This file is only to accommodate for old pickled files, so they can still be unpickled by GraphCreator.
+"""
+
+from tools.download.article_scraper import RawArticle
