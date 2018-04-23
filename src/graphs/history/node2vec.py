051669046b238b807385087b4c5653785629741d
Magdalena Strzoda <deiidrell@gmail.com>
2018-04-23 23:55:28 +0200

    Added code for making simple visualisations of nodes (2d, 3d) for Node2Vec embeddings.

--- src/graphs/history/node2vec.py
+++ src/graphs/history/node2vec.py
@@ -3,4 +3,8 @@ from tools.analysis.embedding.node2vec import SnapNode2Vec
 from graphs.history.wiki_config import HISTORY_DATA_DIR
 
 if __name__ == '__main__':
-    SnapNode2Vec(HISTORY_DATA_DIR, 'graph.graph', dimensions=3).create_embedding()
+    node2vec = SnapNode2Vec(HISTORY_DATA_DIR, 'graph.graph', dimensions=2)
+    # node2vec.create_embedding()
+
+    embedding = node2vec.load_embedding()
+    node2vec.visualise()
--- src/tools/analysis/embedding/node2vec.py
+++ src/tools/analysis/embedding/node2vec.py
@@ -1,5 +1,8 @@
 from subprocess import call
 
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 from settings import NODE2VEC_EXT
 from file_operations import get_filepath
 
@@ -24,6 +27,7 @@ class SnapNode2Vec:
         self.context_size = context_size
         self.max_iter = max_iter
         self.is_weighted = is_weighted
+        self.embedding = None
 
     def create_embedding(self):
         args = [
@@ -48,3 +52,46 @@ class SnapNode2Vec:
             print(str(e))
 
         print("Created embedding successfully and saved it to {}.".format(self.embedding_filename))
+
+    def load_embedding(self):
+        with open(self.embedding_filename, "r") as f:
+            lines = f.read().splitlines()
+            first_line = lines[0]
+            graph_size = int(first_line[0])
+            dimensions = int(first_line[1])
+            print(graph_size, dimensions)
+
+            embedding = []
+
+            for line in lines[1:]:
+                splitted = line.split()
+                id = splitted[0]
+                coordinates = splitted[1:]
+                embedding.append({
+                    "id": id,
+                    "coordinates": [float(coord) for coord in coordinates]
+                })
+
+            self.embedding = embedding
+            return embedding
+
+    def visualise(self):
+        if not self.embedding:
+            raise Exception("No embedding loaded!")
+
+        coordinates_list = [node['coordinates'] for node in self.embedding]
+        coordinates = []
+        for dim in range(self.dimensions):
+            coordinates.append([x[dim] for x in coordinates_list])
+
+        fig = plt.figure()
+        if self.dimensions == 2:
+            plt.scatter(coordinates[0], coordinates[1])
+        elif self.dimensions == 3:
+            ax = fig.add_subplot(111, projection='3d')
+            ax.scatter(coordinates[0], coordinates[1], coordinates[2])
+            ax.set_xlabel('X Label')
+            ax.set_ylabel('Y Label')
+            ax.set_zlabel('Z Label')
+
+        plt.show()
