1876f4178b4c84ffafd42d5a5039a8694a67ef18
Magdalena Strzoda <deiidrell@gmail.com>
2018-05-06 12:49:44 +0200

    Refactored LinkPredictor to split into multiple functions.

--- src/tools/analysis/embedding/grepl.py
+++ src/tools/analysis/embedding/grepl.py
@@ -12,10 +12,12 @@ class LinkPredictor:
 
     def __init__(self, graph):
         self.graph = graph.to_undirected()  # This algorithm doesn't work for directed graphs?
-        self.common_neighbours_node_pairs = []
+        self.common_neighbours_node_pairs = self._get_common_neighbour_node_pairs()
         self.training_nodes = set()
         self.test_nodes = set()
-        self.link_probability_matrix = [] # make np.array
+        self.rf_classifier = None
+        self.cv_score = 0.0
+        self.link_probability_matrix = {} # make actual matrix?
 
     def _get_common_neighbour_node_pairs(self):
         """
@@ -31,7 +33,7 @@ class LinkPredictor:
                         node_pairs.append((node1, node2))
         return node_pairs
 
-    def _get_positive_and_negative_examples(self, common_neighbour_node_pairs):
+    def _get_positive_and_negative_examples(self):
         """
         Positive Examples: Nodes connected by an edge, that have at lest one common neighbour.
         Negative Examples: Nodes not connected by an edge, that have at lest one common neighbour.
@@ -42,7 +44,7 @@ class LinkPredictor:
         negative_examples = set()
         for node in self.graph.node:
             neighbours_with_common_neighbours = [j for j in self.graph.nodes()
-                                                 if (node, j) in common_neighbour_node_pairs]
+                                                 if (node, j) in self.common_neighbours_node_pairs]
 
             positive_i = {j for j in neighbours_with_common_neighbours if j in self.graph.neighbors(node)}
 
@@ -62,20 +64,15 @@ class LinkPredictor:
                     test_examples.add((node1, node2))
         return test_examples
 
-    def build_predictions_matrix(self):
-        pass
-
-    def get_link_predictions(self):
-        """Training dataset consists of equal numnber of positive (with links) and negative (without links) examples."""
-        common_neighbour_node_pairs = self._get_common_neighbour_node_pairs()
+    def _build_data_frame(self):
         df = pd.DataFrame(columns=['nodes', 'adjusted_degree1', 'adjusted_degree2', 'common_neighbours',
                                    'adamic_adar_score'])
 
-        for node1, node2 in common_neighbour_node_pairs:
-            has_edge = 1 if node1 in self.graph.neighbors(node2) else 0   # value of adjacency matrix for (node1, node2)
+        for node1, node2 in self.common_neighbours_node_pairs:
+            has_edge = 1 if node1 in self.graph.neighbors(node2) else 0
 
+            # Returned result is an one-element list of (node1, node2, score)
             adamic_adar_score_result = list(nx.adamic_adar_index(self.graph, [(node1, node2)]))
-            # It's a one-element list of (node1, node2, score)
             adamic_adar_score = adamic_adar_score_result[0][2]
 
             df = df.append({
@@ -84,54 +81,65 @@ class LinkPredictor:
                 'adjusted_degree2': self.graph.degree(node2) - has_edge,
                 'common_neighbours': len(list(nx.common_neighbors(self.graph, node1, node2))),  # FIXME: Don't calcualte this twice~
                 'adamic_adar_score': adamic_adar_score,
-                'edge': has_edge # FIXME: this is wrong
+                'edge': has_edge
             }, ignore_index=True)
 
         df = df.set_index('nodes')
 
-        positive_examples, negative_examples = self._get_positive_and_negative_examples(common_neighbour_node_pairs)
-        print(positive_examples, negative_examples)
+        return df
 
-        print(len(self.graph.edges()))
-        print(len(positive_examples | negative_examples))
+    def _get_X_and_y(self, df):
+        X = df.drop('edge', axis=1)
+        y = df['edge']
+        return X, y
+
+    def _train_model(self, df):
+        positive_examples, negative_examples = self._get_positive_and_negative_examples()
 
         # Take only rows that are in our positive or negative examples
-        print('DataFrame before filtering', df.shape)
         training_df = df[df.index.isin(positive_examples | negative_examples)]
-        print('DataFrame after filtering', training_df.shape)
+        train_X, train_y = self._get_X_and_y(training_df)
 
-        train_X = training_df.drop('edge', axis=1)
-        train_y = training_df['edge']
+        # print(positive_examples, negative_examples)
+        # print(len(self.graph.edges()))
+        # print(len(positive_examples | negative_examples))
+        # print('DataFrame before filtering', df.shape)
+        # print('DataFrame after filtering', training_df.shape)
 
         # Train RF Classifier
-        rf_classifier = RandomForestClassifier()
-        # rf_classifier = RandomForestRegressor()
-        rf_classifier.fit(train_X, train_y)
+        self.rf_classifier = RandomForestClassifier()
+        self.rf_classifier.fit(train_X, train_y)
 
-        # Cross-validation accuracy of m
+        cv_scores = cross_val_score(self.rf_classifier, train_X, train_y, scoring='accuracy')
+        self.cv_score = cv_scores.mean()
+        print(cv_scores, self.cv_score)
 
-        # scoring = ['accuracy']
-        # cv_result = cross_validate(rf_classifier, train_X, train_y, scoring=scoring)
-        # print(cv_result)
-
-        cv_scores = cross_val_score(rf_classifier, train_X, train_y, scoring='accuracy')
-        mean_cv_score = cv_scores.mean()
-        print(cv_scores, mean_cv_score)
-
+    def _get_predictions(self, df):
         test_examples = self._get_test_examples()
-        test_df = df[df.index.isin(test_examples)]
-        test_X = test_df.drop('edge', axis=1)
-        # test_y = test_df['edge']
 
-        predictions = rf_classifier.predict(test_X)
-        # print(predictions)
-        smoothed_predictions = mean_cv_score * predictions + (1 - mean_cv_score) * (1 - predictions)
-        print(smoothed_predictions)
+        test_df = df[df.index.isin(test_examples)]
+        test_X, _ = self._get_X_and_y(test_df)
+
+        predictions = self.rf_classifier.predict(test_X)
+
+        smoothed_predictions = self.cv_score * predictions + (1 - self.cv_score) * (1 - predictions)
+
+        return test_df, smoothed_predictions
+
+    def build_predictions_matrix(self):
+        """Training dataset consists of equal numnber of positive (with links) and negative (without links) examples."""
+        df = self._build_data_frame()
+
+        self._train_model(df)
+
+        test_df, smoothed_predictions = self._get_predictions(df)
 
         predictions_dict = {}
         for (row_index, row), prediction in zip(test_df.iterrows(), smoothed_predictions):
-            print(row_index, prediction)
             predictions_dict[row_index] = prediction
+
+        self.link_probability_matrix = predictions_dict
+        print(self.link_probability_matrix)
 
 
 class GraphEmbeddingWithPredictedLinks:
@@ -141,25 +149,6 @@ class GraphEmbeddingWithPredictedLinks:
         :param graph: NetworkX graph
         """
         self.graph = graph
-
-    def _get_positive_and_negative_examples_invalid(self, node_pairs):
-        """
-        Positive Examples: Nodes connected by an edge, that have at lest one common neighbour.
-        Negative Examples: Nodes not connected by an edge, that have at lest one common neighbour.
-        :param:node_pairs: list of tuples (i, j) representing nodes
-        :returns: two lists of tuples (i, j) representing nodes
-        """
-
-        positive_examples = set()
-        negative_examples = set()
-        for node1, node2 in node_pairs:
-            if node1 in self.graph.neighbors(node2):
-                positive_examples.add((node1, node2))
-            else:
-                negative_examples.add((node1, node2))
-
-        return positive_examples, negative_examples
-
 
 
 if __name__ == "__main__":
@@ -171,6 +160,6 @@ if __name__ == "__main__":
     # plt.show()
 
     grepl = LinkPredictor(graph)
-    grepl.get_link_predictions()
+    grepl.build_predictions_matrix()
 
 
